06c4f4e48210ddb26ab1cc1fc995f891b7200e1a
Van Hung <vanhungnguyenbku@gmail.com>
2021-12-13 08:41:23 +0700

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-
+import { useDispatch, useSelector } from 'react-redux'
+import { dispathLogin } from './redux/actions/authActions'
 import Header from './components/header/Header'
 import Body from './components/body/Body'
+import axios from 'axios'
 
 function App() {
+	const dispatch = useDispatch()
+	const auth = useSelector((state) => state.auth)
+	const token = useSelector((state) => state.token)
+
+	useEffect(() => {
+		const firstLogin = localStorage.getItem('firstLogin')
+		if (firstLogin) {
+			const getToken = async () => {
+				const res = await axios.post('/user/refresh_token', null)
+
+				dispatch({ type: 'GET_TOKEN', payload: res.data.access_token })
+			}
+			getToken()
+		}
+	}, [auth.isLogged, dispatch])
+
+	useEffect(() => {
+		if (token) {
+			const getUser = () => {
+				dispatch(dispathLogin())
+			}
+			getUser()
+		}
+	}, [token, dispatch])
+
 	return (
 		<Router>
 			<div className='App'>
--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../../assets/images/vanhunglogo.png'
+import { useSelector } from 'react-redux'
+import axios from 'axios'
 
 const AppHeader = () => {
+	const auth = useSelector((state) => state.auth)
+
+	const { user, isLogged } = auth
+
+	const userLink = () => {
+		return (
+			<li>
+				<Link to='/'>
+					<img src={user.avatar} alt='avatar' /> {user.name}
+				</Link>
+			</li>
+		)
+	}
+
 	return (
 		<header>
 			<div className='header__container'>
@@ -17,11 +33,15 @@ const AppHeader = () => {
 							<i className='fas fa-shopping-cart'></i> Cart
 						</Link>
 					</li>
-					<li>
-						<Link to='/login'>
-							<i className='fas fa-user'></i> Sign in
-						</Link>
-					</li>
+					{isLogged ? (
+						userLink()
+					) : (
+						<li>
+							<Link to='/login'>
+								<i className='fas fa-user'></i> Sign in
+							</Link>
+						</li>
+					)}
 				</ul>
 			</div>
 		</header>
